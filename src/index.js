23c22651f5d4fb73bcea8a5ec80ca895bdeeaa38
Derek Bender <djbender@instructure.com>
2017-10-28 15:26:38 -0500

    gh-page initial commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import theme from '@instructure/ui-themes/lib/canvas';
-import './index.css';
-import ComponentStuffer from './ComponentStuffer';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<ComponentStuffer />, document.getElementById('root'));
-registerServiceWorker();
